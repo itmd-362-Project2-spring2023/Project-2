1400b4bff57b03b0a21ea3682c81e8098008cb2e
infinanova <29325066+infinanova@users.noreply.github.com>
2023-04-26 18:34:09 -0500

    Created form validation function

--- script/script.js
+++ script/script.js
@@ -6,3 +6,40 @@ function toggleMenu(){
         navbar.style.display = "none";
     }
 }
+
+function validateForm(){
+    enableSubmit = true;
+    fnameField = document.getElementById("fname");
+    lnameField = document.getElementById("lname");
+    emailField = document.getElementById("email");
+    submitButton = document.getElementById("submit");
+
+    if(fnameField.value == ""){
+        enableSubmit = false;
+        fnameField.classList.add("incorrect");
+    } else {
+        fnameField.classList.remove("incorrect");
+    }
+
+    if(lnameField.value == ""){
+        enableSubmit = false;
+        lnameField.classList.add("incorrect");
+    } else {
+        lnameField.classList.remove("incorrect");
+    }
+
+    if(!(emailField.value.includes("@") && emailField.value.includes(".") && emailField.value.length >= 5)){
+        enableSubmit = false;
+        emailField.classList.add("incorrect");
+    } else {
+        emailField.classList.remove("incorrect");
+    }
+
+    if(enableSubmit){
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+    }
+
+
+}
